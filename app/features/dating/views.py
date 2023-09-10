04afeb375a5b0ea6356575129db64fb7bf495210
unknown <waramity.re@gmail.com>
2023-09-11 01:31:46 +0700

    add socket display notification

--- app/features/dating/views.py
+++ app/features/dating/views.py
@@ -84,7 +84,9 @@ def message(form):
 
 @socketio.on("receivedMessageServer", namespace='/dating')
 def receivedMessage(data):
-    if data["sender_id"] == session.get("currentChat"):
+    if data["sender_id"] == session.get("current_chat"):
+        print("kuy")
         # socketio.emit("displayReceivedMessage", data, room=data["recipientID"])
     else:
-        # socketio.emit("displayNotification", data, room=data["recipientID"])
+        # print("sus")
+        socketio.emit("displayNotification", data, room=data["recipient_id"], namespace='/dating')
