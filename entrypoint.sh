aff3dd105b0311cf9e9a29fc7d2cc219305a1e22
unknown <waramity.re@gmail.com>
2023-09-13 04:45:56 +0700

    change gunicorn command

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-gunicorn --worker-class eventlet -w 4 --bind 143.198.206.234:56730 wsgi:app
+#gunicorn --worker-class eventlet -w 4 --bind 143.198.206.234:56730 wsgi:app
+gunicorn --worker-class eventlet -w 4 --bind 0.0.0.0:8000 wsgi:app
 # uwsgi --http :56730 --gevent 1000 --http-websockets --master --wsgi-file wsgi.py --callable app
