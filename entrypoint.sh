0f7ddcd127eaed5558bcf2d6ff42703a1a101d79
unknown <waramity.re@gmail.com>
2023-09-13 04:02:06 +0700

    change entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-gunicorn --worker-class eventlet -w 4 --bind 143.198.206.234:56730 wsgi:app
+# gunicorn --worker-class eventlet -w 4 --bind 143.198.206.234:56730 wsgi:app
+uwsgi --http :56730 --gevent 1000 --http-websockets --master --wsgi-file wsgi.py --callable app
